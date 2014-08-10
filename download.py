1fd3dca9d2afb9b1ce43094f13368a6148f3ad1a
Youri Ackx <youri@ackx.net>
2014-08-10 18:01:08 +0200

    Removed trivial short argument form for 'id'

--- download.py
+++ download.py
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser(description='Download a SoundCloud tracks and p
 parser.add_argument('--track', '-t', help="Track URL")
 parser.add_argument('--playlist', '-p', help="Public or shared playlist URL")
 parser.add_argument('--all', '-a', help="User URL. Download all tracks for all public playlists")
-parser.add_argument('--id', '-i', help='Client ID', required=True)
+parser.add_argument('--id', help='Client ID', required=True)
 parser.add_argument(
         '--override', '-d', action='store_true',
         help='Override file if it exists. Defaults to false')
